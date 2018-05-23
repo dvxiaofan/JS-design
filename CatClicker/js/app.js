a362d00b2f0628cbc0d10677aaf2c25ac0a526e1
xiaofan <1353815633@qq.com>
2018-05-23 14:34:42 +0800

    knockout - with(one)

--- CatClicker/js/app.js
+++ CatClicker/js/app.js
@@ -1,13 +1,9 @@
-var viewModel = function () {
+var Cat = function () {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('cat01');
 	this.imgSrc = ko.observable('images/cat01.jpg');
 	this.imgAttribution = ko.observable('');
 	this.nicknames = ko.observableArray(['kitty', 'marniy', 'tommi', 'niky', 'lisa']);
-
-	this.incrementCounter = function () {
-		this.clickCount(this.clickCount() + 1);
-	};
 
 	this.title = ko.computed(() => {
 		var title;
@@ -26,8 +22,16 @@ var viewModel = function () {
 			title = 'xiaofan';
 		}
 		return title;
-	})
+	}, this);
 
+}
+
+var viewModel = function () {
+	this.currentCat = ko.observable(new Cat());
+
+	this.incrementCounter = function () {
+		this.clickCount(this.clickCount() + 1);
+	};
 }
 
 // 激活ko
